162fc8cf46d3da4241a3e8c636e82504fce7e71d
Agustín Queija <agustin.queija@gmail.com>
2022-12-27 14:42:21 -0300

    Navbar sol ocon usuario, hamburguesa y menu desplegable de usuario

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,109 +1,180 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
-     LightModeOutlined,
-     DarkModeOutlined,
-     Menu as MenuIcon,
-     Search,
-     SettingsOutlined,
-     ArrowDropDownOutlined,
- } from '@mui/icons-material';
- import FlexBetween from "components/FlexBetween";
- import { useDispatch } from 'react-redux';
- import { setMode } from 'state';
- import profileImage from "../assets/profile.png";
-import { AppBar, Toolbar, useTheme, IconButton, InputBase, Button, Box, Menu, MenuItem, Typography } from '@mui/material';
+  LightModeOutlined,
+  DarkModeOutlined,
+  Menu as MenuIcon,
+  ArrowDropDownOutlined,
+  Language,
+  Person,
+  Logout,
+} from "@mui/icons-material";
+import FlexBetween from "components/FlexBetween";
+import { useDispatch } from "react-redux";
+import { setMode } from "state";
+import {
+  AppBar,
+  Toolbar,
+  useTheme,
+  IconButton,
+  InputBase,
+  Button,
+  Box,
+  Menu,
+  MenuItem,
+  Typography,
+  Icon,
+  ListItemIcon,
+  ListItemText,
+  MenuList,
+  ListItem,
+  Divider,
+} from "@mui/material";
 
+const user = {
+  name: "Maryam",
+  lastName: " Burgess",
+  profile: "Company Administrator",
+  id: 1,
+};
 
-const Navbar = ({
-    user,
-    isSidebarOpen,
-    setIsSidebarOpen,
-}) => {
-    const dispatch = useDispatch();
-    const theme = useTheme();
+const availableLangs = [
+  {
+    key: "es",
+    label: "Español",
+  },
+  {
+    key: "en",
+    label: "Ingles",
+  },
+  {
+    key: "pt",
+    label: "Portugués",
+  },
+];
 
-    const [anchorEl, setAnchorEl] = useState(null);
-    const isOpen = Boolean(anchorEl);
-    const handleClick = (event) => setAnchorEl(event.currentTarget);
-    const handleClose = () => setAnchorEl(null);
+function handleChangeLang(lang) {
+  console.log("Handle change lang to: ", lang);
+}
+
+const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
+  const dispatch = useDispatch();
+  const theme = useTheme();
+  const [showLangs, setShowLangs] = useState(false);
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const isOpen = Boolean(anchorEl);
+  const handleClick = (event) => setAnchorEl(event.currentTarget);
+  const handleClose = () => {
+    setAnchorEl(null);
+    setShowLangs(false);
+  };
+  const handleLogout = () => {
+    console.log("handle logout");
+    handleClose();
+  };
 
   return (
-  <AppBar
-  sx={{
-    position: "static",
-    background: "none",
-    boxShadow: "none",
-  }}
-  >
-    <Toolbar sx={{ justifyContent:"space-between" }}>
+    <AppBar
+      sx={{
+        position: "static",
+        background: "none",
+        boxShadow: "none",
+      }}>
+      <Toolbar sx={{ justifyContent: "space-between" }}>
         {/* LEFT SIDE */}
         <FlexBetween gap="1.5rem">
-            <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
-                <MenuIcon />              
-            </IconButton>
-            <FlexBetween
-                backgroundColor={theme.palette.background.alt}
-                borderRadius="9px"
-                gap="3rem"
-                p="0.1rem 1.5rem"
-            >
-                <InputBase placeholder="Search..." />
-                <IconButton>
-                    <Search />
-                </IconButton>
-            </FlexBetween>
+          <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+            <MenuIcon />
+          </IconButton>
         </FlexBetween>
 
         {/* RIGHT SIDE */}
         <FlexBetween gap="1.5rem">
-            <IconButton onClick={() => dispatch(setMode())}>
-                {theme.palette.mode === 'dark' ? (
-                    <LightModeOutlined sx={{fontSize: "25px"}} />
-                ) : (
-                    <DarkModeOutlined sx={{fontSize: "25px"}} />
-                )}
-            </IconButton>
-            <IconButton>
-                <SettingsOutlined sx={{fontSize: "25px"}} />
-            </IconButton>
+          <FlexBetween>
+            <Button
+              onClick={handleClick}
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                textTransform: "none",
+                gap: "1rem",
+              }}>
+              <Person
+                sx={{
+                  fontSize: "25px",
+                  borderRadius: "100%",
+                  backgroundColor: theme.palette.primary[100],
+                  color: theme.palette.primary[800],
+                }}></Person>
 
-            <FlexBetween>
-                <Button onClick={handleClick} sx={{
-                    display: "flex", 
-                    justifyContent: "space-between", 
-                    alignItems: "center", 
-                    textTransform: "none", 
-                    gap: "1rem"}} >
-                    
-                    <Box
-                            component="img"
-                            alt="profile"
-                            src={profileImage}
-                            height="32px"
-                            width="32px"
-                            borderRadius="50%"
-                            sx={{objectFit: "cover"}}
-                    />
-                    <Box textAlign="left">
-                        <Typography fontWeight="bold" fontSize="0.85rem" sx={{color: theme.palette.secondary[100]}} >
-                            {user.name}
-                        </Typography>
-                        <Typography  fontSize="0.75rem" sx={{color: theme.palette.secondary[100]}} >
-                            {user.occupation}
-                        </Typography>
-                    </Box>
-                    <ArrowDropDownOutlined 
-                    sx={{color: theme.palette.secondary[200]}} />
-                </Button>
-                <Menu anchorEl={anchorEl} open={isOpen} onClose={handleClose} anchorOrigin={{vertical:"bottom", horizontal:"center"}}>
-                    <MenuItem onClick={handleClose}>Log Out</MenuItem>
-                </Menu>
-            </FlexBetween>
-
+              <Box textAlign="left">
+                <Typography
+                  fontWeight="bold"
+                  fontSize="0.85rem"
+                  sx={{ color: theme.palette.secondary[100] }}>
+                  {user.name}
+                </Typography>
+                <Typography
+                  fontSize="0.75rem"
+                  sx={{ color: theme.palette.secondary[100] }}>
+                  {user.profile}
+                </Typography>
+              </Box>
+              <ArrowDropDownOutlined
+                sx={{ color: theme.palette.secondary[200] }}
+              />
+            </Button>
+            <Menu
+              anchorEl={anchorEl}
+              open={isOpen}
+              onClose={handleClose}
+              anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+              transformOrigin={{ vertical: "top", horizontal: "right" }}
+              disableScrollLock={true}>
+              <MenuItem onClick={() => dispatch(setMode())}>
+                <ListItemIcon>
+                  {theme.palette.mode === "dark" ? (
+                    <LightModeOutlined sx={{ fontSize: "25px" }} />
+                  ) : (
+                    <DarkModeOutlined sx={{ fontSize: "25px" }} />
+                  )}
+                </ListItemIcon>
+                <ListItemText>
+                  {theme.palette.mode === "dark" ? "Light Mode" : "Dark Mode"}
+                </ListItemText>
+              </MenuItem>
+              <Divider></Divider>
+              <MenuItem onClick={() => setShowLangs(!showLangs)}>
+                <ListItemIcon>
+                  <Language sx={{ fontSize: "25px" }}></Language>
+                </ListItemIcon>
+                <ListItemText>Idioma</ListItemText>
+              </MenuItem>
+              {showLangs && (
+                <MenuList>
+                  {availableLangs.map((lang) => (
+                    <MenuItem
+                      key={lang.key}
+                      onClick={() => handleChangeLang(lang.key)}>
+                      {lang.label}
+                    </MenuItem>
+                  ))}
+                </MenuList>
+              )}
+              <Divider></Divider>
+              <MenuItem onClick={handleLogout}>
+                <ListItemIcon>
+                  <Logout />
+                </ListItemIcon>
+                <ListItemText>Log Out</ListItemText>
+              </MenuItem>
+            </Menu>
+          </FlexBetween>
         </FlexBetween>
-    </Toolbar>
-  </AppBar>
+      </Toolbar>
+    </AppBar>
   );
-}
+};
 
 export default Navbar;
