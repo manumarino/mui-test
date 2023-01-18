68312734bd8ddc9d1a37a5b7b2cc488637f47811
Agustín Queija <agustin.queija@gmail.com>
2023-01-18 15:17:09 -0300

    dejar un solo modal

--- client/src/scenes/companies/index.jsx
+++ client/src/scenes/companies/index.jsx
@@ -4,89 +4,34 @@ import { useGetProductsQuery } from "state/api";
 import Header from "components/Header";
 import DebFormModal from "components/DebFormModal";
 import { DebFormCheckbox, DebFormTextInput, formBuilder } from "components/DebFormComponents";
+import DataTable from "components/DataTable";
 
 function Companies() {
   const [modalState, setModalState] = useState(false);
-  const [modal2State, setModal2State] = useState(false);
-  const openModal = () => {
-    setModalState(true);
-  };
-  const closeModal = () => {
-    setModalState(false);
-  };
-  const formConfig = {
-    onSubmit: (values) => {
-      console.log(values);
-      closeModal();
-    },
-    fields: [
-      {
-        name: "name",
-        label: "NOMBRE",
-        type: "text",
-        initialValue: "",
-      },
-      {
-        name: "direccion",
-        label: "DRI",
-        type: "text",
-        initialValue: "",
-      },
-      {
-        name: "estado",
-        label: "EST",
-        type: "text",
-        initialValue: "",
-      },
-      {
-        name: "capcha",
-        label: "usarCapcha",
-        type: "checkbox",
-        initialValue: false,
-      },
-      {
-        name: "idioma",
-        label: "Idioma",
-        type: "select",
-        initialValue: "",
-        selectOptions: [
-          {label: "Ingles", value: "en"},
-          {label: "Espanol", value: "es"},
-        ]
-      },
-    ],
-  };
+
 
   const handleSubmit = (values) => {
     console.log(values);
   };
 
-  const openModal2 = () => {
-    setModal2State(true);
+  const openModal = () => {
+    setModalState(true);
   };
 
-  const closeModal2 = () => {
-    setModal2State(false);
+  const closeModal = () => {
+    setModalState(false);
   };
   return (
     <Box>
       <Header title="COMPAÑÍAS" subtitle="Listado de compañías" />
+
       <Button variant="contained" onClick={openModal}>
-        Crear Compañía
+        Crear Compañía2
       </Button>
       <DebFormModal
         open={modalState}
         onClose={closeModal}
         onReject={closeModal}
-        formConfig={formConfig}>
-      </DebFormModal>
-      <Button variant="contained" onClick={openModal2}>
-        Crear Compañía2
-      </Button>
-      <DebFormModal
-        open={modal2State}
-        onClose={closeModal2}
-        onReject={closeModal2}
         initialValues={{
           name: "",
           direccion: "",
@@ -102,6 +47,7 @@ function Companies() {
           <DebFormCheckbox label={"usar capcha"} name={"capcha"}/>
         </Stack>
       </DebFormModal>
+      <DataTable></DataTable>
     </Box>
   );
 }
