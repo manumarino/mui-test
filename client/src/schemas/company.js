bb8ca71f4e5f568a380f136923ef795383691aac
manumarino <maezma.ar@gmail.com>
2023-02-06 13:49:17 -0300

    Cambios en validacion

--- client/src/schemas/company.js
+++ client/src/schemas/company.js
@@ -3,9 +3,9 @@ import * as Yup from 'yup';
 export const companyValidationSchema = Yup.object({
   name: Yup.string().required("Nombre es requerido"),
   logo: Yup.string().required("Logo es requerido"),
-  dominio: Yup.string().required("Dominio es requerido"),
-  extension: Yup.string().matches(/^\.\w{2,}$/, "Extensión no es valida").required("Extensión es requerido"),
-  debqUrl: Yup.string().url("La URL no es valida").required("URL del debQ es requerido"),
+  dominio: Yup.string().required("Dominio es requerido. Especifique si es URL"),
+  extension: Yup.string().matches(/^\.\w{2,}$/, "Extensión no es válida. Debe comenzar con un punto, seguido de dos letras.").required("Extensión es requerido"),
+  debqUrl: Yup.string().url("La URL no es válida. Debe comenzar con 'http://'").required("URL del debQ es requerido"),
   debqUser: Yup.string().required("Usuario del debQ es requerido"),
   debqPassword: Yup.string().required("Contraseña del debQ es requerido"),
   timeZone: Yup.string().required("Zona horaria es requerido"),
--- client/src/schemas/mobile.js
+++ client/src/schemas/mobile.js
@@ -1,13 +1,13 @@
 import * as Yup from "yup";
 
 export const mobileValidationSchema = Yup.object({
-  urlMobile: Yup.string().url("La URL no es valida").required("URL de Mobile es requerido"),
+  urlMobile: Yup.string().url("La URL no es válida").required("URL de Mobile es requerido"),
   surveyCompany: Yup.object({ id: Yup.string().required("Compañía de encuestas es requerido") }),
   userMobile: Yup.string().required("Usuario es requerido"),
   passwordMobile: Yup.string()
   .min(8, "Contraseña debe contener al menos 8 caracteres")
   .matches(/\w/, "Contraseña debe contener al menos una letra")
-  .matches(/\d/, "Contraseña debe contener al menos un numero")
+  .matches(/\d/, "Contraseña debe contener al menos un número")
   .required("Contraseña de Mobile es requerido"),
   tokenMobile: Yup.string().required("Token de Mobile es requerido"),
 });
